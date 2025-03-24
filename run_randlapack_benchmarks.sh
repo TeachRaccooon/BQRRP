7581346be3bd4e1185c1a0072df8ac994c095f0d
TeachRaccooon <m.y.melnich@gmail.com>
2025-03-24 11:44:27 -0700

    CPU stuff works

--- run_randlapack_benchmarks.sh
+++ run_randlapack_benchmarks.sh
@@ -17,9 +17,6 @@ if [[ ! -d "$BENCHMARK_OUTPUT_DIR" ]]; then
     echo "Adding variable $BENCHMARK_OUTPUT_DIR to ~/.bashrc."
     echo "#Added via run_randlapack_benhcmarks.sh" >> ~/.bashrc
     echo "export BENCHMARK_OUTPUT_DIR=\"$RANDNLA_PROJECT_DIR/build/benchmark-build/benchmark-output\"" >> ~/.bashrc
-    # Reload bashrc to make the new variable visible
-    #bash -c "source setvars.sh --force && source ~/.bashrc && exec bash"
-    bash -c "source ~/.bashrc && exec bash"
 fi
 
 # Attempt to grab the CPU name
@@ -29,49 +26,49 @@ if [[ -z "$CPU_NAME" ]]; then
     echo "Failed to retrieve CPU name. Naming it after today's date."
     CPU_NAME=$(date +"%Y-%m-%d")
 fi
-CPU_NAME="$BENCHMARK_OUTPUT_DIR/$CPU_NAME"
+CPU_DIR="$BENCHMARK_OUTPUT_DIR/$CPU_NAME"
 
 # Create the directory named after the current CPU 
-if [[ ! -d "$$CPU_NAME" ]]; then
-    mkdir -p "$CPU_NAME"
-    echo "Directory created at: $CPU_NAME"
+if [[ ! -d "$$CPU_DIR" ]]; then
+    mkdir -p "$CPU_DIR"
+    echo "Directory created at: $CPU_DIR"
 fi
 # Create the directories for specific BQRRP benchmarks
-if [[ ! -d "$CPU_NAME/HQRRP_plot_remake" ]]; then
-    mkdir -p "$CPU_NAME/HQRRP_plot_remake"
-    echo "Directory created at: $CPU_NAME/HQRRP_plot_remake"
+if [[ ! -d "$CPU_DIR/HQRRP_plot_remake" ]]; then
+    mkdir -p "$CPU_DIR/HQRRP_plot_remake"
+    echo "Directory created at: $CPU_DIR/HQRRP_plot_remake"
 fi
-if [[ ! -d "$CPU_NAME/BQRRP_subroutines_speed" ]]; then
-    mkdir -p "$CPU_NAME/BQRRP_subroutines_speed"
-    echo "Directory created at: $CPU_NAME/BQRRP_subroutines_speed"
+if [[ ! -d "$CPU_DIR/BQRRP_subroutines_speed" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_subroutines_speed"
+    echo "Directory created at: $CPU_DIR/BQRRP_subroutines_speed"
 fi
-if [[ ! -d "$CPU_NAME/BQRRRP_runtime_breakdown" ]]; then
-    mkdir -p "$CPU_NAME/BQRRRP_runtime_breakdown"
-    echo "Directory created at: $CPU_NAME/BQRRRP_runtime_breakdown"
+if [[ ! -d "$CPU_DIR/BQRRP_runtime_breakdown" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_runtime_breakdown"
+    echo "Directory created at: $CPU_DIR/BQRRP_runtime_breakdown"
 fi
-if [[ ! -d "$CPU_NAME/BQRRP_speed_comparisons_block_size" ]]; then
-    mkdir -p "$CPU_NAME/BQRRP_speed_comparisons_block_size"
-    echo "Directory created at: $CPU_NAME/BQRRP_speed_comparisons_block_size"
+if [[ ! -d "$CPU_DIR/BQRRP_speed_comparisons_block_size" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_speed_comparisons_block_size"
+    echo "Directory created at: $CPU_DIR/BQRRP_speed_comparisons_block_size"
 fi
-if [[ ! -d "$CPU_NAME/BQRRP_speed_comparisons_mat_size" ]]; then
-    mkdir -p "$CPU_NAME/BQRRP_speed_comparisons_mat_size"
-    echo "Directory created at: $CPU_NAME/BQRRP_speed_comparisons_mat_size"
+if [[ ! -d "$CPU_DIR/BQRRP_speed_comparisons_mat_size" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_speed_comparisons_mat_size"
+    echo "Directory created at: $CPU_DIR/BQRRP_speed_comparisons_mat_size"
 fi
-if [[ ! -d "$CPU_NAME/HQRRP_speed_comparisons_block_size" ]]; then
-    mkdir -p "$CPU_NAME/HQRRP_speed_comparisons_block_size"
-    echo "Directory created at: $CPU_NAME/HQRRP_speed_comparisons_block_size"
+if [[ ! -d "$CPU_DIR/HQRRP_speed_comparisons_block_size" ]]; then
+    mkdir -p "$CPU_DIR/HQRRP_speed_comparisons_block_size"
+    echo "Directory created at: $CPU_DIR/HQRRP_speed_comparisons_block_size"
 fi
-if [[ ! -d "$CPU_NAME/HQRRP_runtime_breakdown" ]]; then
-    mkdir -p "$CPU_NAME/HQRRP_runtime_breakdown"
-    echo "Directory created at: $CPU_NAME/HQRRP_runtime_breakdown"
+if [[ ! -d "$CPU_DIR/HQRRP_runtime_breakdown" ]]; then
+    mkdir -p "$CPU_DIR/HQRRP_runtime_breakdown"
+    echo "Directory created at: $CPU_DIR/HQRRP_runtime_breakdown"
 fi
-if [[ ! -d "$CPU_NAME/BQRRP_speed_comparisons_block_size_small" ]]; then
-    mkdir -p "$CPU_NAME/BQRRP_speed_comparisons_block_size_small"
-    echo "Directory created at: $CPU_NAME/BQRRP_speed_comparisons_block_size_small"
+if [[ ! -d "$CPU_DIR/BQRRP_speed_comparisons_block_size_small" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_speed_comparisons_block_size_small"
+    echo "Directory created at: $CPU_DIR/BQRRP_speed_comparisons_block_size_small"
 fi
-if [[ ! -d "$CPU_NAME/BQRRP_speed_comparisons_mat_size_rectangular" ]]; then
-    mkdir -p "$CPU_NAME/BQRRP_speed_comparisons_mat_size_rectangular"
-    echo "Directory created at: $CPU_NAME/BQRRP_speed_comparisons_mat_size_rectangular"
+if [[ ! -d "$CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular" ]]; then
+    mkdir -p "$CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular"
+    echo "Directory created at: $CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular"
 fi
 
 # Ask the user if they want to continue or terminate the script
@@ -85,7 +82,7 @@ else
     # Check the number of CPU sockets
     CPU_SOCKETS=$(grep -c 'physical id' /proc/cpuinfo)
     # Determine the maximum number of OpenMP threads that can be used on a given system
-    MAX_THREADS=$((2*CPU_SOCKETS*(nproc --all)))
+    MAX_THREADS=$((2 * CPU_SOCKETS * $(nproc --all)))
     
     THREADS_LIST=(1)
     # Exceptional case used for the AMD system with exceptionally large core count
@@ -116,51 +113,85 @@ else
             fi
         fi
     fi
+    
+    # Make a shortcut variable
+    EXECUTION_DIR=$RANDNLA_PROJECT_DIR/build/benchmark-build
 
     # Get the benchmark commencement date
-    DATETIME=$(date "+%Y-%m-%d %H:%M:%S")
+    DATETIME=$(date "+%Y-%m-%d-%H:%M:%S")
 
     # PERFORMING BENCHMARK RUNS
     # HQRRP plot remake benchmarks
     for ITEM in "${THREADS_LIST[@]}"; do
-        echo "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM ./BQRRP_speed_comparisons_mat_size $CPU_NAME/HQRRP_plot_remake/$DATETIME hqrrp_const 20 1 1 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000;"
+        echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM EXECUTION_DIR/BQRRP_speed_comparisons_mat_size CPU_DIR/HQRRP_plot_remake/$DATETIME hqrrp_const 1 1 1 1000;"
+        eval "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM $EXECUTION_DIR/BQRRP_speed_comparisons_mat_size $CPU_DIR/HQRRP_plot_remake/$DATETIME hqrrp_const 1 1 1 1000;"
     done
+    echo -e "HQRRP plot remake benchmarks complete\n"
 
     # BQRRP subroutines speed
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_subroutines_speed $CPU_NAME/BQRRP_subroutines_speed/$DATETIME 3 65536 256 512 1024 2048 4096 8192"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_subroutines_speed $CPU_NAME/BQRRP_subroutines_speed/$DATETIME 3 64000 250 500 1000 2000 4000 8000;"
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_subroutines_speed CPU_DIR/BQRRP_subroutines_speed/$DATETIME 1 1024 256;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_subroutines_speed $CPU_DIR/BQRRP_subroutines_speed/$DATETIME 1 1024 256;"
+
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_subroutines_speed CPU_DIR/BQRRP_subroutines_speed/$DATETIME 1 1000 250;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_subroutines_speed $CPU_DIR/BQRRP_subroutines_speed/$DATETIME 1 1000 250;"
+    echo -e "BQRRP subroutines speed complete\n"
 
     # BQRRP runtime breakdown
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_runtime_breakdown $CPU_NAME/BQRRRP_runtime_breakdown/$DATETIME cholqr 3 65536 65536 256 512 1024 2048 4096 8192;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_runtime_breakdown $CPU_NAME/BQRRRP_runtime_breakdown/$DATETIME geqrf  3 65536 65536 256 512 1024 2048 4096 8192;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_runtime_breakdown $CPU_NAME/BQRRRP_runtime_breakdown/$DATETIME cholqr 3 64000 64000 250 500 1000 2000 4000 8000;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_runtime_breakdown $CPU_NAME/BQRRRP_runtime_breakdown/$DATETIME geqrf  3 64000 64000 250 500 1000 2000 4000 8000;"
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_runtime_breakdown CPU_DIR/BQRRP_runtime_breakdown/$DATETIME cholqr 1 1024 1024 256;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_runtime_breakdown $CPU_DIR/BQRRP_runtime_breakdown/$DATETIME cholqr 1 1024 1024 256;"
+    
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_runtime_breakdown CPU_DIR/BQRRP_runtime_breakdown/$DATETIME geqrf  1 1024 1024 256;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_runtime_breakdown $CPU_DIR/BQRRP_runtime_breakdown/$DATETIME geqrf  1 1024 1024 256;"
+    
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_runtime_breakdown CPU_DIR/BQRRP_runtime_breakdown/$DATETIME cholqr 1 1000 1000 250;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_runtime_breakdown $CPU_DIR/BQRRP_runtime_breakdown/$DATETIME cholqr 1 1000 1000 250;"
+    
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_runtime_breakdown CPU_DIR/BQRRP_runtime_breakdown/$DATETIME geqrf  1 1000 1000 250;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_runtime_breakdown $CPU_DIR/BQRRP_runtime_breakdown/$DATETIME geqrf  1 1000 1000 250;"
+    echo -e "BQRRP runtime breakdown complete\n"
 
     # BQRRP performance varying block size
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_block_size $CPU_NAME/BQRRP_speed_comparisons_block_size/$DATETIME default 3 65536 65536 256 512 1024 2048 4096 8192;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_block_size $CPU_NAME/BQRRP_speed_comparisons_block_size/$DATETIME default 3 64000 64000 250 500 1000 2000 4000 8000;"
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_speed_comparisons_block_size CPU_DIR/BQRRP_speed_comparisons_block_size/$DATETIME default 1 1024 1024 256;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_speed_comparisons_block_size $CPU_DIR/BQRRP_speed_comparisons_block_size/$DATETIME default 1 1024 1024 256;"
+
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_speed_comparisons_block_size CPU_DIR/BQRRP_speed_comparisons_block_size/$DATETIME default 1 1000 1000 250;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_speed_comparisons_block_size $CPU_DIR/BQRRP_speed_comparisons_block_size/$DATETIME default 1 1000 1000 250;"
+    echo -e "BQRRP performance varying block size complete\n"
 
     # BQRRP performance varying mat size
     for ITEM in "${THREADS_LIST[@]}"; do
-        echo "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM ./BQRRP_speed_comparisons_mat_size $CPU_NAME/BQRRP_speed_comparisons_mat_size/$DATETIME default 3 1 32 0 8000 16000 32000;"
+        echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM EXECUTION_DIR/BQRRP_speed_comparisons_mat_size CPU_DIR/BQRRP_speed_comparisons_mat_size/$DATETIME default 1 1 32 1000;"
+        eval "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM $EXECUTION_DIR/BQRRP_speed_comparisons_mat_size $CPU_DIR/BQRRP_speed_comparisons_mat_size/$DATETIME default 1 1 32 1000;"
     done
+    echo -e "BQRRP performance varying mat size complete\n"
 
     # HQRRP performance varying block size
     for ITEM in "${THREADS_LIST[@]}"; do
-        echo "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM ./BQRRP_speed_comparisons_block_size $CPU_NAME/HQRRP_speed_comparisons_block_size/$DATETIME hqrrp 3 32000 32000 5 10 25 50 125 250 500 1000 2000 4000 8000;"
+        echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM EXECUTION_DIR/BQRRP_speed_comparisons_block_size CPU_DIR/HQRRP_speed_comparisons_block_size/$DATETIME hqrrp 1 1000 1000 5;"
+        eval "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM $EXECUTION_DIR/BQRRP_speed_comparisons_block_size $CPU_DIR/HQRRP_speed_comparisons_block_size/$DATETIME hqrrp 1 1000 1000 5;"
     done
+    echo -e "HQRRP performance varying block size complete\n"
 
     # HQRRP runtime breakdown
     for ITEM in "${THREADS_LIST[@]}"; do
-        echo "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM ./HQRRP_runtime_breakdown $CPU_NAME/HQRRP_runtime_breakdown/$DATETIME 3 32000 32000 5 10 25 50 125 250 500 1000 2000 4000 8000;"
+        echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM EXECUTION_DIR/HQRRP_runtime_breakdown CPU_DIR/HQRRP_runtime_breakdown/$DATETIME 1 1000 1000 5;"
+        eval "$NUMACTL_VAR env OMP_NUM_THREADS=$ITEM $EXECUTION_DIR/HQRRP_runtime_breakdown $CPU_DIR/HQRRP_runtime_breakdown/$DATETIME 1 1000 1000 5;"
     done
+    echo -e "HQRRP runtime breakdown complete\n"
 
     # BQRRP performance varying block size small
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_block_size $CPU_NAME/BQRRP_speed_comparisons_block_size_small/$DATETIME default 3 1000 1000 5 10 25 50 125 250 500 1000;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_block_size $CPU_NAME/BQRRP_speed_comparisons_block_size_small/$DATETIME default 3 2000 2000 5 10 25 50 125 250 500 1000 2000;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_block_size $CPU_NAME/BQRRP_speed_comparisons_block_size_small/$DATETIME default 3 4000 4000 5 10 25 50 125 250 500 1000 2000 4000;"
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_speed_comparisons_block_size CPU_DIR/BQRRP_speed_comparisons_block_size_small/$DATETIME default 1 1000 1000 5;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_speed_comparisons_block_size $CPU_DIR/BQRRP_speed_comparisons_block_size_small/$DATETIME default 1 1000 1000 5;"
+    echo -e "BQRRP performance varying block size small complete\n"
 
     # BQRRP performance varying mat size rectangular: tall and wide
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_mat_size $CPU_NAME/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 3 2   32 0 8000 16000 32000 64000;"
-    echo "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS ./BQRRP_speed_comparisons_mat_size $CPU_NAME/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 3 0.5 32 0 8000 16000 32000 64000;"
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_speed_comparisons_mat_size CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 1 2   32 1000;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_speed_comparisons_mat_size $CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 1 2   32 1000;"
+
+    echo -e "\n$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS EXECUTION_DIR/BQRRP_speed_comparisons_mat_size CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 1 0.5 32 1000;"
+    eval "$NUMACTL_VAR env OMP_NUM_THREADS=$MAX_THREADS $EXECUTION_DIR/BQRRP_speed_comparisons_mat_size $CPU_DIR/BQRRP_speed_comparisons_mat_size_rectangular/$DATETIME default_hqrrp_const 1 0.5 32 1000;"
+    echo -e "BQRRP performance varying mat size rectangular: tall and wide complete\n"
 fi
+
+# Source from bash and spawn a new shell so that the variable change takes place
+bash -c "source ~/.bashrc && exec bash"
